f9cf7080d8485b3f4fb18fb2f403c6714d6ef814
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-12-19 00:08:27 -0700

    WPF Grid exploration. an implementation of Tick Tac Toe game

--- WPF_Grid_C-/App.xaml.cs
+++ WPF_Grid_C-/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WPF_Gri_C_
+namespace WPF_Grid_C_
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- WPF_Grid_C-/MainWindow.xaml.cs
+++ WPF_Grid_C-/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_Gri_C_
+namespace WPF_Grid_C_
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
